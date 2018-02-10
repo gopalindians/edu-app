0cba27a5380af5a6731a6135ffc71fb5bd3b7345
Gopal Sharma <gopalindians@gmail.com>
2018-02-10 12:08:47 +0530

    Added nav bar menu active for login page

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -9,10 +9,10 @@ defined( 'BASEPATH' ) OR exit( 'No direct script access allowed' );
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
-
     <meta name='keywords' content='your, tags'>
     <meta name='description' content='150 words'>
-    <meta name='subject' content='your website's subject'>
+    <meta name='subject' content='your website' s subject
+    '>
     <meta name='copyright' content='company name'>
     <meta name='language' content='ES'>
     <meta name='robots' content='index,follow'>
@@ -28,7 +28,8 @@ defined( 'BASEPATH' ) OR exit( 'No direct script access allowed' );
     <meta name='url' content='http://www.websiteaddrress.com'>
     <meta name='identifier-URL' content='http://www.websiteaddress.com'>
     <meta name='directory' content='submission'>
-    <meta name='pagename' content='jQuery Tools, Tutorials and Resources - O'Reilly Media'>
+    <meta name='pagename' content='jQuery Tools, Tutorials and Resources - O' Reilly Media
+    '>
     <meta name='category' content=''>
     <meta name='coverage' content='Worldwide'>
     <meta name='distribution' content='Global'>
@@ -56,7 +57,6 @@ defined( 'BASEPATH' ) OR exit( 'No direct script access allowed' );
     <meta http-equiv='x-dns-prefetch-control' content='off'>
 
 
-
     <link rel='alternate' type='application/rss+xml' title='RSS' href='http://feeds.feedburner.com/martini'>
     <link rel='alternate' type='application/atom+xml' title='Atom 0.3' href='https://example.com/feed.atom'>
     <link rel='shortcut icon' type='image/ico' href='/favicon.ico'>
@@ -66,7 +66,7 @@ defined( 'BASEPATH' ) OR exit( 'No direct script access allowed' );
     <link rel='archives' title='May 2003' href='http://blog.unto.net/2003/05/'>
     <link rel='index' title='DeWitt Clinton' href='http://blog.unto.net/'>
     <link rel='start' title='Pattern Recognition 1' href='http://blog.unto.net/photos/pattern_recognition_1_about/'>
-    <link rel='bookmark'title='Styleguide' href='http://paulrobertlloyd.com/about/styleguide/'>
+    <link rel='bookmark' title='Styleguide' href='http://paulrobertlloyd.com/about/styleguide/'>
     <link rel='search' href='/search.xml' type='application/opensearchdescription+xml' title='Viatropos'>
 
     <link rel='self' type='application/atom+xml' href='http://www.syfyportal.com/atomFeed.php?page=3'>
@@ -78,8 +78,9 @@ defined( 'BASEPATH' ) OR exit( 'No direct script access allowed' );
     <link rel='canonical' href='http://smallbiztrends.com/2010/06/9-things-to-do-before-entering-social-media.html'>
     <link rel='EditURI' type='application/rsd+xml' title='RSD' href='http://smallbiztrends.com/xmlrpc.php?rsd'>
     <link rel='pingback' href='http://smallbiztrends.com/xmlrpc.php'>
-<!--    <link rel='stylesheet' media='only screen and (max-device-width: 480px)' href='http://wordpress.org/style/iphone.css' type='text/css'>-->
-    <link rel='wlwmanifest' href='http://www.example.com/wp-includes/wlwmanifest.xml' type='application/wlwmanifest+xml'>
+    <!--    <link rel='stylesheet' media='only screen and (max-device-width: 480px)' href='http://wordpress.org/style/iphone.css' type='text/css'>-->
+    <link rel='wlwmanifest' href='http://www.example.com/wp-includes/wlwmanifest.xml'
+          type='application/wlwmanifest+xml'>
 
     <link rel='help' title='FAQ' href='/faq'>
     <link rel='logo' type='image/svg' href='https://playfoursquare.s3.amazonaws.com/press/logo/foursquare-logo.svg'>
@@ -88,7 +89,8 @@ defined( 'BASEPATH' ) OR exit( 'No direct script access allowed' );
     <link rel='image_src' href='http://du3itj18e4z0b.cloudfront.net/7b29fe/images/icon-facebook.gif' type='image/jpeg'>
 
     <link rel='author' href='humans.txt' type='text/plain'>
-    <link href='http://thenextweb.com/2009/01/08/how-to-snap-up-that-twitter-username-youve-always-wanted/' rel='original-source'>
+    <link href='http://thenextweb.com/2009/01/08/how-to-snap-up-that-twitter-username-youve-always-wanted/'
+          rel='original-source'>
     <link rel='profile' title='Microformats' href='http://microformats.org/profile/specs/'>
     <link rel='profile' href='http://gmpg.org/xfn/11'>
 
@@ -106,7 +108,7 @@ defined( 'BASEPATH' ) OR exit( 'No direct script access allowed' );
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
-                <li class="nav-item active">
+                <li class="nav-item">
                     <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
@@ -115,23 +117,31 @@ defined( 'BASEPATH' ) OR exit( 'No direct script access allowed' );
                 <li class="nav-item">
                     <a class="nav-link" href="#">Pricing</a>
                 </li>
-                <li class="nav-item dropdown">
-					<?php if ( $this->session->has_userdata( 'UE' ) ): ?>
-
+				<?php if ( $this->session->has_userdata( 'UE' ) ): ?>
+                    <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
                            aria-haspopup="true" aria-expanded="false">
 							<?php echo $this->session->userdata()['UE']; ?>
                         </a>
-
                         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
                             <a class="dropdown-item" href="#">Profile</a>
                             <a class="dropdown-item" href="#">Another action</a>
                             <a class="dropdown-item" href="/auth/logout">Logout</a>
                         </div>
+                    </li>
+				<?php else: ?>
+
+					<?php if ( $this->uri->segment( 2 ) === 'login' ): ?>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="/auth/login"> Login</a>
+                        </li>
 					<?php else: ?>
-                        <a class="nav-link" href="/auth/login"> Login</a>
+                        <li class="nav-item">
+                            <a class="nav-link" href="/auth/login"> Login</a>
+                        </li>
 					<?php endif; ?>
-                </li>
+				<?php endif; ?>
+
             </ul>
         </div>
     </nav>
